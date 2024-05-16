dad0a3140fc37d1f68583e923252dc46ca0fa268
Robert Crummett <rncrummett5@gmail.com>
2024-05-15 18:07:03 -0700

    Added Logo dir

---
+++ Figure/mask_recolor.py
@@ -0,0 +1,32 @@
+'''
+Simple script to make all non transparent pixels black
+
+Author: R Nate Crummett, 05/2024
+'''
+from pathlib import Path
+
+import matplotlib.image as mpimg
+
+#######################################
+# Path to image, .jpg used in example
+image_path = Path('Figure/mines-temp.png')
+output_path = Path('Figure/mines-logo-light.png')
+
+#######################################
+#
+#      No Edits Below This Line
+#
+#######################################
+# Load image
+rgb_255 = mpimg.imread(image_path)
+
+# Mask non transparent areas
+rgb_mask = rgb_255[...,-1] != 0
+
+# Set all non transparent values to black
+rgb_255[rgb_mask,:-1] = 1
+
+##########################################
+# Save output image
+mpimg.imsave(output_path, rgb_255)
+
