c6828018e037581e226d1d3d950a3b6a5a21208c
Robert Crummett <rncrummett5@gmail.com>
2024-07-13 13:02:31 -0700

    Removed as much lua as possible

--- bib.lua
+++ bib.lua
@@ -2,8 +2,20 @@
 -- Wonderful idea by Skuost, Dec 5 2023
 -- https://tex.stackexchange.com/questions/192917/include-all-bib-files-in-one-directory-to-a-bibliography
 
+-- Function checks whether directory exists or not
+local function isDir(name)
+	if type(name) ~= "string" then
+		return false
+	end
+	local cd = lfs.currentdir()
+	local is = lfs.chdir(name) and true or false
+	lfs.chdir(cd)
+	return is
+end
+
+
 -- Lua function to collect *.bib files in Bib directory
-function findBibFiles(folder)
+local function findBibFiles(folder)
 	local files = {}
 	local lfs = require("lfs")
 	if isDir(folder) then
@@ -16,19 +28,8 @@ function findBibFiles(folder)
 	return files
 end
 
--- Function checks whether directory exists or not
-function isDir(name)
-	if type(name) ~= "string" then
-		return false
-	end
-	local cd = lfs.currentdir()
-	local is = lfs.chdir(name) and true or false
-	lfs.chdir(cd)
-	return is
-end
-
 -- Function assembles bibliography
-function printBibResources(folder)
+local function printBibResources(folder)
 	-- Get all bib files in folder
 	local files = findBibFiles(folder)
 
